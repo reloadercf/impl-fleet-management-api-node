6c9b084d67394e4463d3023fe9cf60b2e7bd2dae
reloadercf <elcfm1@gmail.com>
2024-03-25 23:40:46 -0600

    install dev dependecies

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,10 @@
+import express from 'express';
+import path from 'path';
+
+interface Options{
+    PORT: number;
+    PUBLIC_PAth: string;
+}
 class Server {
     public static start() {
         console.log('Server started...');
