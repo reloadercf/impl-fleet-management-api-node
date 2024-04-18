b1524fb2b4f450a7e3b1e8494b8f62f394c61c66
reloadercf <elcfm1@gmail.com>
2024-04-17 21:26:06 -0600

    feat: :wrench: add query params of consult

--- src/presentation/taxi/taxiController.ts
+++ src/presentation/taxi/taxiController.ts
@@ -1,9 +1,28 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { type Request, type Response } from 'express'
 import { prisma } from '../../data/postgres'
 
 export class TaxiController {
   public getTaxis = async (req: Request, res: Response) => {
-    const allTaxis = await prisma.taxis.findMany()
-    res.json(allTaxis)
+    if (Object.keys(req.query).length === 0) {
+      const allTaxis = await prisma.taxis.findMany()
+      return res.json(allTaxis)
+    }
+    if (req.query.id) {
+      const getTaxiById = await prisma.taxis.findUnique({
+        where: {
+          id: +req.query.id
+        }
+      })
+      return res.json(getTaxiById)
+    }
+    if (req.query.plate) {
+      const getTaxiByPlate = await prisma.taxis.findMany({
+        where: {
+          plate: `${req.query.plate}`
+        }
+      })
+      return res.json(getTaxiByPlate)
+    }
   }
 }
